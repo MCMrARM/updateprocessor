d3f74e5a9225be711ce368339fc4d1d97758afcf
MCMrARM <mcmrarm@gmail.com>
2019-03-21 23:06:17 +0100

    main.cpp: Enable the win10 versiondb updater

--- main.cpp
+++ main.cpp
@@ -1,19 +1,25 @@
 #include <iostream>
 #include <fstream>
+#include <git2.h>
 
 #include "play_manager.h"
 #include "discord_state.h"
 #include "telegram_state.h"
 #include "win10_store_manager.h"
+#include "win10_versiondb_manager.h"
 
 
 int main() {
     std::cout << "Starting up!" << std::endl;
 
+    git_libgit2_init();
+
     PlayManager playManager;
     ApkManager apkManager (playManager);
     Win10StoreManager win10Manager;
     win10Manager.init();
+    Win10VersionDBManager win10VdbManager;
+    win10VdbManager.addWin10StoreMgr(win10Manager);
 
     //playManager.getDeviceARM().downloadApk("com.mojang.minecraftpe", 871021311, "priv/test.apk");
     static DiscordState* discordState = new DiscordState(playManager, apkManager);
